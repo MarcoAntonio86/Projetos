c610bfa0b17b050b4e84e06b1bfb327855a21c41
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2023-08-01 11:16:04 -0300

    primeira atualização 01/08

--- login/login.js
+++ login/login.js
@@ -1,1 +1,28 @@
+const enviarBtn = document.getElementById('Enviar');
+const camposObrigatorios = document.querySelectorAll('[required]');
 
+function verificarCamposObrigatorios() {
+  let todosPreenchidos = true;
+
+  camposObrigatorios.forEach(campo => {
+    if (!campo.value) {
+      todosPreenchidos = false;
+    }
+  });
+
+  if (todosPreenchidos) {
+    enviarBtn.classList.add('enviar-btn-estilizado');
+  } else {
+    enviarBtn.classList.remove('enviar-btn-estilizado');
+  }
+}
+
+enviarBtn.addEventListener('click', () => {
+  const todosCamposPreenchidos = Array.from(camposObrigatorios).every(campo => campo.value.trim() !== '');
+
+  if (todosCamposPreenchidos) {
+    enviarBtn.classList.add('enviar-btn-estilizado');
+  }
+});
+
+document.addEventListener('input', verificarCamposObrigatorios);
