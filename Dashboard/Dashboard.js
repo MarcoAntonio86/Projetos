43bb7ca74242cb93963d1026f3c119589470be1b
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2023-08-03 11:40:21 -0300

    Primeira alteração 03/08

---
+++ Dashboard/Dashboard.js
@@ -0,0 +1,9 @@
+document.getElementById("botao_vendas").addEventListener("click", function() {
+
+   var vendasMes = document.getElementById("vendasMes").value;
+   var vendasTotal = document.getElementById("vendasTotal").value;
+
+   
+});
+
+
